0356b15e209e1da61b7253088646a22e7272c244
Joe Kaldas <joekaldas8@gmail.com>
2020-09-07 01:06:34 +0200

    chore: precompile

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,12 @@ module RriStaffing
     # the framework and any gems in your application.
 
     config.time_zone = 'Cairo'
-    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    # config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)$/
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)$/
+    config.assets.precompile << Proc.new { |filename, path|
+      !filename.include?("/")
+    }
+    config.generators.javascript_engine = :js
     config.autoload_paths += %W(#{config.root}/lib) # add this line
     config.generators.javascript_engine = :js
     I18n.default_locale = :en
